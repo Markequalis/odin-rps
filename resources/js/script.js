b499bcfc1128a74c5ed3c0982abb3f32dc1c72bd
Markequalis <johnathon.taylor@rynercorp.com>
2024-09-20 14:35:38 -0500

    Added a header title and game description that says when the game ends. Increased border-radiuses from 5px to 10px. Changed footer to include the link to Freepik as well as my branding.

--- resources/js/script.js
+++ resources/js/script.js
@@ -1,12 +1,13 @@
-document.addEventListener('DOMContentLoaded', () => {
-    const allowedChoices = ['rock', 'paper', 'scissors'];
+document.addEventListener('DOMContentLoaded', () => {   // I wanted to prevent the script from loading before the page content.
+    const allowedChoices = ['rock', 'paper', 'scissors']; 
 
-    const getComputerChoice = () => {
+    const getComputerChoice = () => {   // This allows the game to generate a selection for the computer.
         const i = Math.floor(Math.random() * 3);
         return allowedChoices[i];
     }
-
-    const playRound = (humanChoice, computerChoice) => {
+    
+    // This is what the results are depending on the player/computer selections.
+    const playRound = (humanChoice, computerChoice) => { 
         humanChoice = humanChoice.toLowerCase();
         let result = '';
 
@@ -22,7 +23,7 @@ document.addEventListener('DOMContentLoaded', () => {
             result = computerChoice === 'rock' ? 'Player loses! Rock beats scissors!' : 'Player wins! Scissors beats paper!';
         }
 
-        
+        // This changes the image based on what the player/computer chooses.
         document.getElementById('pchoiceimg').src = `resources/images/${humanChoice}.png`;
         document.getElementById('cchoiceimg').src = `resources/images/${computerChoice}.png`;
 
@@ -33,7 +34,7 @@ document.addEventListener('DOMContentLoaded', () => {
     let computerScore = 0;
     let currentRoundNumber = 1;
 
-    const choicesContainer = document.getElementById('choices');
+    const choicesContainer = document.getElementById('choices'); // This keeps track of the score as well as resets the game.
     choicesContainer.addEventListener('click', (event) => {
         if (event.target.tagName === 'BUTTON') {
             const humanChoice = event.target.getAttribute('data-choice');
@@ -41,7 +42,8 @@ document.addEventListener('DOMContentLoaded', () => {
             const result = playRound(humanChoice, computerChoice);
 
             document.getElementById('rnumber').textContent = currentRoundNumber;
-
+            
+            // This decides which score increases.
             if (result.includes('wins')) {
                 humanScore++;
             } else if (result.includes('loses')) {
@@ -54,24 +56,25 @@ document.addEventListener('DOMContentLoaded', () => {
             resultReasonElement.textContent = result;
             resultReasonElement.style.display = 'block';
 
+            // The code blow sets what happens when the game is over.
             if (humanScore === 5 || computerScore === 5) {
                 let finalResult = `Final scores: Player: ${humanScore}, Computer: ${computerScore}. `;
                 if (humanScore > computerScore) {
                     finalResult = 'The player wins overall!';
                 } else {
                     finalResult = 'The computer wins overall!';
-                }
+                } 
                 document.getElementById('results').textContent = `Final scores: Player: ${humanScore}, Computer: ${computerScore}`;
                 document.getElementById('final-result').textContent = finalResult;
-                
                 resultReasonElement.style.display = 'none';
 
+                // The code below helps reset how the page looks once the game restarts.
                 humanScore = 0;
                 computerScore = 0;
                 currentRoundNumber = 1;
-            } else {
+            } else { 
                 document.getElementById('results').textContent = `Player: ${humanScore} - Computer: ${computerScore}`;
-                document.getElementById('final-result').textContent = '';
+                document.getElementById('final-result').textContent = ''; 
             }
         }
     });
